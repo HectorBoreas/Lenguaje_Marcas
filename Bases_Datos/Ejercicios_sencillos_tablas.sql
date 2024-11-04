640ba74b8c84530d854d59c0a3f3b97396ac141a
HectorBoreas <hector_pp_18@yahoo.es>
2024-11-04 16:14:42 +0000

    bbdd_completo

--- Bases_Datos/Ejercicios_sencillos_tablas.sql
+++ Bases_Datos/Ejercicios_sencillos_tablas.sql
@@ -1,5 +1,5 @@
 # Crear Nueva Base de datos
-CREATE DATABASE IF NOT EXISTS Tablas_Sencillas CHARACTER SET utf8_ci;
+CREATE DATABASE IF NOT EXISTS Tablas_Sencillas CHARACTER SET utf8;
 USE Tablas_Sencillas;
 
 # Ejercicio 1
@@ -26,7 +26,6 @@ CREATE TABLE Productos (
 CREATE TABLE Pedidos (
     id_pedido INT PRIMARY KEY AUTO_INCREMENT,
     fecha DATE,
-    -- Revisar antes de ejecutar
     fk_id_usuario INT
 );
 
@@ -124,7 +123,7 @@ CREATE TABLE Metodos_pagos (
 -- Ejercicio 15
 /* Ejercicio 15 */
 -- Revisar antes de ejcutar
-ALTER TABLE Pedidos ADD fk_id_pedido INT;
+ALTER TABLE Pedidos ADD metodo_pago_id INT;
 
 # Ejercicio 16
 -- Ejercicio 16
@@ -151,7 +150,6 @@ CREATE TABLE Valoraciones (
     id_valoriacion INT PRIMARY KEY AUTO_INCREMENT,
     fk_id_producto INT,
     fk_id_usuario INT,
-    -- Revisar INT, valor entre 1 y 5
     calificacion INT CHECK(calificacion >= 1 AND calificacion<=5),
     comentario TEXT(200)
 );
@@ -182,25 +180,26 @@ CREATE TABLE Historial_Pedidos (
 
 # Agregar clave foranea de usuario a tabla de direcciones
 
-#ALTER TABLE Direcciones ADD CONSTRAINT fk_id_usuario FOREIGN KEY(fk_id_usuario) REFERENCES Usuarios(id_usuario);
+ALTER TABLE Direcciones ADD CONSTRAINT fk_Direcciones_Usuarios FOREIGN KEY(fk_id_usuario) REFERENCES Usuarios(id_usuario);
 
-#ALTER TABLE Historial_Pedidos ADD CONSTRAINT fk_id_pedido FOREIGN KEY(fk_id_pedido) REFERENCES Pedidos(id_pedido);
+ALTER TABLE Historial_Pedidos ADD CONSTRAINT fk_Historial_Pedidos_Pedidos FOREIGN KEY(fk_id_pedido) REFERENCES Pedidos(id_pedido);
 
-ALTER TABLE Historial_Precios ADD fk_id_pedido INT FOREIGN KEY REFERENCES Pedidos(id_pedido);
-#ALTER TABLE Historial_Precios ADD CONSTRAINT fk_id_pedido FOREIGN KEY(fk_id_pedido) REFERENCES Pedidos(id_pedido);
+ALTER TABLE Historial_Precios ADD COLUMN fk_id_pedido INT;
+ALTER TABLE Historial_Precios ADD CONSTRAINT fk_Historial_Precios_Pedidos FOREIGN KEY (fk_id_pedido) REFERENCES Pedidos(id_pedido);
 
-#ALTER TABLE Pedidos ADD CONSTRAINT fk_id_usuario FOREIGN KEY(fk_id_usuario) REFERENCES Usuarios(id_usuario);
+ALTER TABLE Pedidos ADD CONSTRAINT fk_Pedidos_Usuarios FOREIGN KEY(fk_id_usuario) REFERENCES Usuarios(id_usuario);
 
-ALTER TABLE Productos ADD CONSTRAINT fk_id_categoria FOREIGN KEY(fk_id_categoria) REFERENCES Categorias(id_categoria);
-ALTER TABLE Productos ADD CONSTRAINT fk_id_proveedor FOREIGN KEY(fk_id_proveedor) REFERENCES Proveedors(id_proveedor);
+ALTER TABLE Productos ADD CONSTRAINT fk_Productos_Categorias FOREIGN KEY(fk_id_categoria) REFERENCES Categorias(id_categoria);
+ALTER TABLE Productos ADD CONSTRAINT fk_Productos_Proveedores FOREIGN KEY(fk_id_proveedor) REFERENCES Proveedores(id_proveedor);
 
-ALTER TABLE Reseñas ADD CONSTRAINT fk_id_producto FOREIGN KEY(fk_id_producto) REFERENCES Productos(id_producto);
-ALTER TABLE Reseñas ADD CONSTRAINT fk_id_usuario FOREIGN KEY(fk_id_usuario) REFERENCES Usuarios(id_usuario);
+ALTER TABLE Valoraciones ADD CONSTRAINT fk_Valoraciones_Productos FOREIGN KEY(fk_id_producto) REFERENCES Productos(id_producto);
+ALTER TABLE Valoraciones ADD CONSTRAINT fk_Valoraciones_Usuarios FOREIGN KEY(fk_id_usuario) REFERENCES Usuarios(id_usuario);
 
-#ALTER TABLE Stocks ADD CONSTRAINT fk_id_producto FOREIGN KEY(fk_id_producto) REFERENCES Productos(id_producto);
+ALTER TABLE Stocks ADD CONSTRAINT fk_Stocks_Productos FOREIGN KEY(fk_id_producto) REFERENCES Productos(id_producto);
 
-ALTER TABLE Wishlists ADD CONSTRAINT fk_id_producto FOREIGN KEY(fk_id_producto) REFERENCES Productos(id_producto);
-ALTER TABLE Wishlists ADD CONSTRAINT fk_id_usuario FOREIGN KEY(fk_id_usuario) REFERENCES Usuarios(id_usuario);
+ALTER TABLE Wishlists ADD CONSTRAINT fk_Wishlist_Productos FOREIGN KEY(fk_id_producto) REFERENCES Productos(id_producto);
+ALTER TABLE Wishlists ADD CONSTRAINT fk_Wishlist_Usuarios FOREIGN KEY(fk_id_usuario) REFERENCES Usuarios(id_usuario);
+
 
 
 
